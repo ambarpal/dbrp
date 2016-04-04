58165fc26e8e2c842351bda1a49bb3f60c33e42c
Harshvardhan <harshvardhan14043@iiitd.ac.in>
2016-04-04 15:52:06 +0530

    Added file uploader with saving features

--- DBMS_Project/src/com/dbms/dbrp/controllers/SearchUploadController.java
+++ DBMS_Project/src/com/dbms/dbrp/controllers/SearchUploadController.java
@@ -3,11 +3,14 @@
  */
 package com.dbms.dbrp.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 /**
  * @author ambar14012, harshvardhan14043
@@ -18,9 +21,15 @@ public class SearchUploadController {
 	@FXML private TextArea authors_u;
 	@FXML private TextArea abstract_u;
 	@FXML private TextArea citations_u;
-	@FXML void uploadPaper(ActionEvent e){
+	@FXML void uploadPaper(ActionEvent e) throws IOException{
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Open paper");
-		fc.showOpenDialog(new Stage());
+		int count = 1;
+		File file = fc.showOpenDialog(null), cp = new File("data/" + count++ + ".pdf");
+		if (file != null) {
+			if (file.toString().endsWith("pdf")) {
+				Files.copy(file.toPath(), cp.toPath());
+			}
+		}
 	}
 }
