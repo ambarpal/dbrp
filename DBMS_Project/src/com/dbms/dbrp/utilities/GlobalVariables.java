bdac5f261029fcd199c5823cc26b2bdac69a8bf9
ambarpal <palambar.96@gmail.com>
2016-04-04 14:29:42 +0530

    Fixed gitignore

--- DBMS_Project/src/com/dbms/dbrp/utilities/GlobalVariables.java
+++
@@ -1,7 +0,0 @@
-package com.dbms.dbrp.utilities;
-
-public class GlobalVariables {
-	public static final String DB_URL = "jdbc:mysql://localhost/";
-	public static final String USER = "root";
-	public static final String PASS = "";
-}
