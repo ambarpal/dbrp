6ec1f9f152a45477741754f7b556c4219bf20355
ambarpal <palambar.96@gmail.com>
2016-04-08 21:06:27 +0530

    Added Relationships and Create User View

--- DBMS_Project/src/com/dbms/dbrp/controllers/SearchUploadController.java
+++ DBMS_Project/src/com/dbms/dbrp/controllers/SearchUploadController.java
@@ -3,7 +3,11 @@
  */
 package com.dbms.dbrp.controllers;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.security.MessageDigest;
@@ -48,7 +52,11 @@ public class SearchUploadController {
 	StringBuffer sb;
 	Node source; 
     Stage stage;
+    Boolean isUploaded;
     
+    public SearchUploadController() throws SQLException{
+    	initVariables();
+	}
 	int initVariables() throws SQLException
 	{
 		conn = DriverManager.getConnection(GlobalVariables.DB_URL, GlobalVariables.USER, GlobalVariables.PASS);
@@ -60,19 +68,28 @@ public class SearchUploadController {
 			if(dbname.equalsIgnoreCase("dbmsProject"))
 				flag = 1;
 		}
+		isUploaded = false;
 		return flag;
 	}
 	
 	@FXML void uploadPaper(ActionEvent e) throws IOException, SQLException
 	{
-		initVariables();
+    	title_u.setText(":)");
+    	authors_u.setText("::)");
+    	abstract_u.setText("::))");
+    	citations_u.setText(":/");
+    	conference_u.setText(":'(");
 		FileChooser fc = new FileChooser();
 		fc.setTitle("Open paper");
-		int count = 1;
+		int count = Integer.parseInt(new BufferedReader(new FileReader("data/counter")).readLine().toString());
 		File file = fc.showOpenDialog(null), cp = new File("data/" + count++ + ".pdf");
 		if (file != null) {
 			if (file.toString().endsWith("pdf")) {
 				Files.copy(file.toPath(), cp.toPath());
+				isUploaded = true;
+				System.out.println(isUploaded);
+				uploadLabel.setText("PDF uploaded");
+				uploadLabel.setTextFill(Color.GREEN);
 			}
 			else
 			{
@@ -80,20 +97,28 @@ public class SearchUploadController {
 				uploadLabel.setTextFill(Color.RED);
 			}
 		}
+		System.out.println(isUploaded);
+		BufferedWriter br = new BufferedWriter(new FileWriter("data/counter"));
+		br.write(count+"");
+		br.close();
 	}
 	
 	@FXML void submit(ActionEvent e) throws SQLException{
+		System.out.println(isUploaded);
 		String title = title_u.getText();
 		String[] authorList = authors_u.getText().split(",");
 		String[] citationList = citations_u.getText().split("\n");
 		String abstractText = abstract_u.getText();
 		String conference = conference_u.getText();
 		Boolean flag2 = (title.length()==0 || authorList.length == 0 || conference.length() == 0 || abstractText.length() == 0 ||  citationList.length == 0);
-		initVariables();
 		if(flag2 == true)
 		{
 			uploadLabel.setText("Enter all fields before submitting");
 			uploadLabel.setTextFill(Color.RED);
+		}
+		else if (isUploaded.booleanValue() == false){
+			uploadLabel.setText("You need to submit a pdf");
+			uploadLabel.setTextFill(Color.RED);	
 		}
 		else
 		{
@@ -104,31 +129,46 @@ public class SearchUploadController {
 //			int id=1;
 			if(!rs.next())
 			{
-				System.out.println("potato");
+				// Tables
 				sql = "CREATE TABLE papers "
-						+"(tid INTEGER PRIMARY KEY, "
+						+"(pid INTEGER PRIMARY KEY, "
 						+"title VARCHAR(400), "
-						+"author VARCHAR(400), "
-						+"keywords VARCHAR(400), "
-						+"conference VARCHAR(400), "
-						+"citationcount INTEGER, "
-						+"citations VARCHAR(1000));"; 
+						+"citationcount INTEGER);"; 
 				stmt.executeUpdate(sql);
-				sql = "CREATE TABLE keywords "
-						+"(tid INTEGER PRIMARY KEY, "
+				sql = "CREATE TABLE keyword "
+						+"(kid INTEGER PRIMARY KEY, "
 						+"word VARCHAR(40), "
 						+"frequency INTEGER);";
 				stmt.executeUpdate(sql);
 				sql = "CREATE TABLE conference "
-						+"(name VARCHAR(400) PRIMARY KEY, "
-						+"date DATE PRIMARY KEY, "
-						+"tid INTEGER);";
+						+"(cid INTEGER PRIMARY KEY, "
+						+"name VARCHAR(400), "
+						+"date DATE);";
 				stmt.executeUpdate(sql);
 				sql = "CREATE TABLE author "
-						+"(name VARCHAR(400) PRIMARY KEY, "
-						+"affiliation VARCHAR(400), "
-						+"address VARCHAR(400);";
+						+"(aid INTEGER PRIMARY KEY, "
+						+"name VARCHAR(400), "
+						+"affiliation VARCHAR(400));";
 				stmt.executeUpdate(sql);
+				
+				// Relationships
+				sql = "CREATE TABLE isAuthorOf "
+						+"(pid INTEGER, "
+						+"aid INTEGER);"; 
+				stmt.executeUpdate(sql);
+				
+				sql = "CREATE TABLE isCitationOf "
+						+"(pid INTEGER, "
+						+"pid INTEGER);"; 
+				stmt.executeUpdate(sql);	
+
+				sql = "CREATE TABLE isPublishedin "
+						+"(pid INTEGER, "
+						+"cid INTEGER);"; 
+				stmt.executeUpdate(sql);
+			}
+			else{
+				
 			}
 		}
 		
